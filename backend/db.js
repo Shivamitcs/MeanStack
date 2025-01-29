28f743f4cac6f56611669e46b4405fdf241ac4a9
Shivamitcs <test8386911@gmail.com>
2025-01-29 16:19:14 +0530

    Update connection string

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/meanDB', (err)=>{
-    if(!err){
-        console.log('Database Connection Successful')
-    }else{
-        console.log('Error in Connection' + err)
-    }
+const mongoURI = 'mongodb+srv://dashrath:<db_password>@cluster0.qur6s.mongodb.net/meanDB?retryWrites=true&w=majority&appName=Cluster0' || 'mongodb://localhost:27017/meanDB';
+
+mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 })
+.then(() => console.log('Database Connection Successful'))
+.catch(err => console.log('Error in Connection:', err));
 
 module.exports = mongoose;
